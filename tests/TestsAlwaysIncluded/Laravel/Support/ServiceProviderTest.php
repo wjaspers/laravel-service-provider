3e8372ac3fcba838f905b5faccaeea2e7e3191ee
Will Jaspers <wjaspers4@gmail.com>
2015-03-29 16:31:14 -0500

    Stub out test class.

---
+++ tests/TestsAlwaysIncluded/Laravel/Support/ServiceProviderTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace TestsAlwaysIncluded\Laravel\Support;
+
+/**
+ * @covers TestsAlwaysIncluded\Laravel\Support\ServiceProvider
+ */
+class ServiceProviderTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * 
+	 */
+	public function testBindServices()
+	{
+		$this->markTestIncomplete(__METHOD__);
+	}
+}
